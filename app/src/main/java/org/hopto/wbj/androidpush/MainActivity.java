11b5e59d9cedff9e1e5d739d00c3224cccd930b1
necro-wbj <d04220837@gm.tut.edu.tw>
2018-12-03 12:13:24 +0800

    Http連線套件volley

--- app/src/main/java/org/hopto/wbj/androidpush/MainActivity.java
+++ app/src/main/java/org/hopto/wbj/androidpush/MainActivity.java
@@ -4,6 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.lang.ref.ReferenceQueue;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG ="necro-wbj";
@@ -22,5 +34,34 @@ public class MainActivity extends AppCompatActivity {
         }
         //通常直接\取值
         // bd.get("某個KEY")
+
+        //連線 請求 回應處理 物件
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest req = new StringRequest(
+                Request.Method.GET,
+                "http:10.184.12.54/test.txt",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG,response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG,error.toString());
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> p = new HashMap<String, String>();
+                p.put("t","之前的Token");
+                p.put("n","名字");
+                return p;
+            }
+        };
+        queue.add(req);
+
     }
 }
--- creat.sql
+++ creat.sql
@@ -1,4 +1,6 @@
+mysql -u root;
 create database test1 default character set utf8mb4 collate utf8mb4_general_ci;
+use test1;
 create table user(
     id int UNSIGNED NOT NUll AUTO_INCREMENT,
     name varchar(200) NOT NUll,
